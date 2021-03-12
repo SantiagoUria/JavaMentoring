28b9eaedc8b517ffb678591824c051a61a88afab
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2021-03-12 11:47:32 -0300

    agrega ejericios de herencia y cadenas de texto

---
+++ src/Herencia/Gato.java
@@ -0,0 +1,46 @@
+package Herencia;
+public class Gato {
+
+    private String nombre;
+    private String raza;
+    private String color;
+    private int edad;
+
+    public void comer(){
+        System.out.println(this.nombre + " está comiendo.");
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    };
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    
+    public Gato(){}
+}
---
+++ src/Herencia/Perro.java
@@ -0,0 +1,7 @@
+package Herencia;
+public class Perro extends Gato {
+    
+public Perro(){}
+
+
+}
