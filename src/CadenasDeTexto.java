f0ecb0b2cabb79b9d744b2514b881d94396c77b0
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2021-03-12 12:03:10 -0300

    cambia los comentarios de ejercicios

--- src/CadenasDeTexto.java
+++ src/CadenasDeTexto.java
@@ -1,6 +1,10 @@
 import java.util.StringTokenizer;
 
 public class CadenasDeTexto {
+
+
+    // Comentar todos los ejercicios excepto el que quieras ver para verlo mejor
+
     public static void main(String[] args) throws Exception {
 
         String cancionP1 = "\nElla existió solo en un sueño \ny él es el poema que el poeta nunca escribió. \nY en la eternidad los dos \nunieron sus almas para darle vida \na esta triste canción de amor.";
@@ -16,22 +20,22 @@ public class CadenasDeTexto {
 
         // Ejercicio 1
 
-        // StringTokenizer c1 = new StringTokenizer(cancionP1);
-        // StringTokenizer c2 = new StringTokenizer(cancionP2);
-        // StringTokenizer c3 = new StringTokenizer(cancionP3);
+        StringTokenizer c1 = new StringTokenizer(cancionP1);
+        StringTokenizer c2 = new StringTokenizer(cancionP2);
+        StringTokenizer c3 = new StringTokenizer(cancionP3);
 
-        // System.out.println("Parte 1: " + cancionP1);
-        // System.out.println("**********************");
-        // System.out.println("Esta parte de la canción tiene " + c1.countTokens() + "palabras");
-        // System.out.println("**********************");
-        // System.out.println("Parte 2: " + cancionP2);
-        // System.out.println("**********************");
-        // System.out.println("Esta parte de la canción tiene " + c2.countTokens() + "palabras");
-        // System.out.println("**********************");
-        // System.out.println("Parte 3: " + cancionP3);
-        // System.out.println("**********************");
-        // System.out.println("Esta parte de la canción tiene " + c3.countTokens() + "palabras");
-        // System.out.println("**********************");
+        System.out.println("Parte 1: " + cancionP1);
+        System.out.println("**********************");
+        System.out.println("Esta parte de la canción tiene " + c1.countTokens() + "palabras");
+        System.out.println("**********************");
+        System.out.println("Parte 2: " + cancionP2);
+        System.out.println("**********************");
+        System.out.println("Esta parte de la canción tiene " + c2.countTokens() + "palabras");
+        System.out.println("**********************");
+        System.out.println("Parte 3: " + cancionP3);
+        System.out.println("**********************");
+        System.out.println("Esta parte de la canción tiene " + c3.countTokens() + "palabras");
+        System.out.println("**********************");
 
 
 
@@ -44,53 +48,53 @@ public class CadenasDeTexto {
 
         
 
-        // for (int x = 0; x < cancion.length(); x++) {
+        for (int x = 0; x < cancion.length(); x++) {
 
-        //     char cancionMinuscula = cancion.toLowerCase().charAt(x);
-        //     switch (cancionMinuscula) {
-        //     case 'a':
-        //         a++;
-        //         break;
-        //     case 'e':
-        //         e++;
-        //         break;
-        //     case 'i':
-        //         i++;
-        //         break;
-        //     case 'o':
-        //         o++;
-        //         break;
-        //     case 'u':
-        //         u++;
-        //         break;
-        //     case '.':
-        //         punto++;
-        //         break;
-        //     case 'á':
-        //         acentos++;
-        //         break;
-        //     case 'é':
-        //         acentos++;
-        //         break;
-        //     case 'í':
-        //         acentos++;
-        //         break;
-        //     case 'ó':
-        //         acentos++;
-        //         break;
-        //     case 'ú':
-        //         acentos++;
-        //         break;
-        //     default:
-        //         consonante++;
-        //         break;
-        //     }
-        // }
-        // int vocales = a + e + i + o + u;
+            char cancionMinuscula = cancion.toLowerCase().charAt(x);
+            switch (cancionMinuscula) {
+            case 'a':
+                a++;
+                break;
+            case 'e':
+                e++;
+                break;
+            case 'i':
+                i++;
+                break;
+            case 'o':
+                o++;
+                break;
+            case 'u':
+                u++;
+                break;
+            case '.':
+                punto++;
+                break;
+            case 'á':
+                acentos++;
+                break;
+            case 'é':
+                acentos++;
+                break;
+            case 'í':
+                acentos++;
+                break;
+            case 'ó':
+                acentos++;
+                break;
+            case 'ú':
+                acentos++;
+                break;
+            default:
+                consonante++;
+                break;
+            }
+        }
+        int vocales = a + e + i + o + u;
 
-        // System.out.println("La cancion tiene:\n" + vocales + " VOCALES \n" + a + " letras A\n" + e + " letras E\n" + i
-        //         + " letras I\n" + o + " letras O\n" + u + " letras U\n" + acentos + " acentos\n" + consonante
-        //         + " consonantes y " + punto + " puntos");
+        System.out.println("La cancion tiene:\n" + vocales + " VOCALES \n" + a + " letras A\n" + e + " letras E\n" + i
+                + " letras I\n" + o + " letras O\n" + u + " letras U\n" + acentos + " acentos\n" + consonante
+                + " consonantes y " + punto + " puntos");
 
 
 
@@ -100,12 +104,12 @@ public class CadenasDeTexto {
 
 
 
-        // String nuevaCancion = cancion.toLowerCase();
-        // nuevaCancion = nuevaCancion.replaceAll("él", "ella");
-        // nuevaCancion = nuevaCancion.replaceAll("ella", "él");
-        // nuevaCancion = nuevaCancion.replace("triste canción de amor", "alegre canción de rock");
-        // nuevaCancion = nuevaCancion.replaceAll("los dos", "el par");
-        // System.out.println("Nueva canción:");
-        // System.out.println(nuevaCancion);
+        String nuevaCancion = cancion.toLowerCase();
+        nuevaCancion = nuevaCancion.replaceAll("él", "ella");
+        nuevaCancion = nuevaCancion.replaceAll("ella", "él");
+        nuevaCancion = nuevaCancion.replace("triste canción de amor", "alegre canción de rock");
+        nuevaCancion = nuevaCancion.replaceAll("los dos", "el par");
+        System.out.println("Nueva canción:");
+        System.out.println(nuevaCancion);
     }
 }
